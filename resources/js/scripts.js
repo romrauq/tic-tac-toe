fb641652fb3c0a15fcb3b0505c3424acc4cf36ef
John Quarmor <johnequarmor@gmail.com>
2020-05-31 08:28:51 +0000

    Added function to update text within play squares

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -8,15 +8,25 @@ Area6 = document.querySelector("#six");
 Area7 = document.querySelector("#seven");
 Area8 = document.querySelector("#eight");
 Area9 = document.querySelector("#nine");
+
 let allAreas = document.querySelectorAll(".inner-square");
 // areaArray = [Area1, Area2, Area3, Area4, Area5, Area6, Area7, Area8, Area9];
 
+let playCount = 0;
+
 for (i = 0; i < allAreas.length; i++) {
-  // areaArray[i].addEventListener("click", playMove);
-  allAreas[i].addEventListener("click", playMove);
+   // areaArray[i].addEventListener("click", playMove);
+   allAreas[i].addEventListener("click", playMove);
 }
 
 function playMove(e) {
-  //   alert(e.target.id);
-  e.target.textContent = "Success!";
+   for (i = 0; i < allAreas.length; i++) {
+      if (playCount % 2 == 0) {
+         e.target.textContent = "X";
+      } else {
+         e.target.textContent = "O";
+      }
+   }
+   playCount++;
+   console.log(playCount);
 }
