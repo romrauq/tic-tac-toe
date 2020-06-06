39de49fa06c1421368d4651eb1295ffb7e30ccf0
John Quarmor <johnequarmor@gmail.com>
2020-06-06 08:29:24 +0000

    function commented out for reset

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -37,69 +37,68 @@ function playMove(e) {
       }
    }
    playCount++;
-   winCondition();
    console.log(playCount);
 }
 
-function winCondition() {
-   if (one.innerText && two.innerText && three.innerText === "X") {
-      alert("Player 1 Wins!!!");
-   } else if (one.innerText && two.innerText && three.innerText === "O") {
-      alert("Player 2 Wins!!!");
-   }
+// function winCondition() {
+//    if (one.innerText && two.innerText && three.innerText === "X") {
+//       alert("Player 1 Wins!!! [win con:'X.123']");
+//    } else if (one.innerText && two.innerText && three.innerText === "O") {
+//       alert("Player 2 Wins!!! [win con:'O.123']");
+//    }
 
-   if (four.innerText && five.innerText && six.innerText === "X") {
-      alert("Player 1 Wins!!!");
-   } else if (four.innerText && five.innerText && six.innerText === "O") {
-      alert("Player 2 Wins!!!");
-   }
+//    if (four.innerText && five.innerText && six.innerText === "X") {
+//       alert("Player 1 Wins!!! [win con:'X.456']");
+//    } else if (four.innerText && five.innerText && six.innerText === "O") {
+//       alert("Player 2 Wins!!! [win con:'0.456']");
+//    }
 
-   if (seven.innerText && eight.innerText && nine.innerText === "X") {
-      alert("Player 1 Wins!!!");
-   } else if (seven.innerText && eight.innerText && nine.innerText === "O") {
-      alert("Player 2 Wins!!!");
-   }
+//    if (seven.innerText && eight.innerText && nine.innerText === "X") {
+//       alert("Player 1 Wins!!! [win con:'X.789']");
+//    } else if (seven.innerText && eight.innerText && nine.innerText === "O") {
+//       alert("Player 2 Wins!!! [win con:'O.789']");
+//    }
 
-   if (one.innerText && four.innerText && seven.innerText === "X") {
-      alert("Player 1 Wins!!!");
-   } else if (one.innerText && four.innerText && seven.innerText === "O") {
-      alert("Player 2 Wins!!!");
-   }
+//    if (one.innerText && four.innerText && seven.innerText === "X") {
+//       alert("Player 1 Wins!!! [win con:'X.147']");
+//    } else if (one.innerText && four.innerText && seven.innerText === "O") {
+//       alert("Player 2 Wins!!! [win con:'O.147']");
+//    }
 
-   if (two.innerText && five.innerText && eight.innerText === "X") {
-      alert("Player 1 Wins!!!");
-   } else if (one.innerText && four.innerText && seven.innerText === "O") {
-      alert("Player 2 Wins!!!");
-   }
+//    if (two.innerText && five.innerText && eight.innerText === "X") {
+//       alert("Player 1 Wins!!! [win con:'X.258']");
+//    } else if (one.innerText && four.innerText && seven.innerText === "O") {
+//       alert("Player 2 Wins!!! [win con:'O.258']");
+//    }
 
-   if (three.innerText && six.innerText && nine.innerText === "X") {
-      alert("Player 1 Wins!!!");
-   } else if (three.innerText && six.innerText && nine.innerText === "O") {
-      alert("Player 2 Wins!!!");
-   }
-   if (one.innerText && five.innerText && nine.innerText === "X") {
-      alert("Player 1 Wins!!!");
-   } else if (one.innerText && five.innerText && nine.innerText === "O") {
-      alert("Player 2 Wins!!!");
-   }
+//    if (three.innerText && six.innerText && nine.innerText === "X") {
+//       alert("Player 1 Wins!!! [win con:'X.369']");
+//    } else if (three.innerText && six.innerText && nine.innerText === "O") {
+//       alert("Player 2 Wins!!! [win con:'O.369']");
+//    }
+//    if (one.innerText && five.innerText && nine.innerText === "X") {
+//       alert("Player 1 Wins!!! [win con:'X.156']");
+//    } else if (one.innerText && five.innerText && nine.innerText === "O") {
+//       alert("Player 2 Wins!!! [win con:'O.159']");
+//    }
 
-   if (three.innerText && five.innerText && seven.innerText === "X") {
-      alert("Player 1 Wins!!!");
-   } else if (one.innerText && five.innerText && seven.innerText === "O") {
-      alert("Player 2 Wins!!!");
-   }
+//    if (three.innerText && five.innerText && seven.innerText === "X") {
+//       alert("Player 1 Wins!!! [win con:'X.357']");
+//    } else if (one.innerText && five.innerText && seven.innerText === "O") {
+//       alert("Player 2 Wins!!! [win con:'O.357']");
+//    }
 
-   function modalOutput() {
-      if (winCondition === "player1") {
-         //Player 1 wins!
-         modal.style.diplay = "block";
-         modalText.innerText = "Player One Wins!!!";
-      } else if (winCondition === "player2") {
-         //Player 2 wins!
-         modal.style.diplay = "block";
-         modalText.innerText = "Player Two Wins!!!";
-      } else if (playCount === 9) {
-         alert("No Winner, Draw!");
-      }
-   }
-}
+//    function modalOutput() {
+//       if (winCondition === "player1") {
+//          //Player 1 wins!
+//          modal.style.diplay = "block";
+//          modalText.innerText = "Player One Wins!!!";
+//       } else if (winCondition === "player2") {
+//          //Player 2 wins!
+//          modal.style.diplay = "block";
+//          modalText.innerText = "Player Two Wins!!!";
+//       } else if (playCount === 9) {
+//          alert("No Winner, Draw!");
+//       }
+//    }
+// }
